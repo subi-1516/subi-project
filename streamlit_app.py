bff9d460b754553967e06177ec7004092b40f986
subi <jusubiksha1675@gmail.com>
2025-05-18 11:58:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,10 @@
 import streamlit as st
 
 def predict_news(text):
-    vec = vectorizer.transform([cleaned])
     pred = rf_model.predict(vec)[0]
     return "Fake" if pred == 1 else "Real"
 
 st.title("Fake News Detector")
 user_input = st.text_area("Enter News Text:")
 if st.button("Predict"):
-    result = predict_news(user_input)
     st.subheader(f"The news is: {result}")
